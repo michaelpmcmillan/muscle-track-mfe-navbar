d86ab09c68da7f1a62ad02eff375c1ab0fc039dc
Michael.McMillan <michael.mcmillan@musclefood.com>
2020-09-21 16:53:36 +0100

    Add home link

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { BrowserRouter, Link } from "react-router-dom";
 
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  return (
+    <BrowserRouter>
+      <nav className="nav">
+        <Link to="/" className="link">
+          Home
+        </Link>
+      </nav>
+    </BrowserRouter>
+  );
 }
