bb4537fa2f4081cc5057c41c47e532c3b2f0e805
Michael.McMillan <michael.mcmillan@musclefood.com>
2020-09-21 16:54:00 +0100

    Add home link test

--- src/root.component.test.tsx
+++ src/root.component.test.tsx
@@ -3,8 +3,8 @@ import { render } from "@testing-library/react";
 import Root from "./root.component";
 
 describe("Root component", () => {
-  it("should be in the document", () => {
-    const { getByText } = render(<Root name="Testapp" />);
-    expect(getByText(/Testapp is mounted!/i)).toBeInTheDocument();
+  it("should render the Home link", () => {
+    const { getByText } = render(<Root />);
+    expect(getByText("Home")).toBeInTheDocument();
   });
 });
