3e1a29a697edefe8e1af0a49441beac36b5dd6e2
Michael.McMillan <michael.mcmillan@musclefood.com>
2020-09-27 09:13:59 +0100

    Implement react-bootstrap header

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { BrowserRouter, Link } from "react-router-dom";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Helmet from "react-helmet";
 
 export default function Root(props) {
   return (
-    <BrowserRouter>
-      <nav className="nav">
-        <Link to="/" className="link">
-          Home
-        </Link>
-      </nav>
-    </BrowserRouter>
+    <Navbar collapseOnSelect bg="light" expand="lg" sticky="top">
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+          integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+          crossOrigin="anonymous"
+        ></link>
+      </Helmet>
+      <Navbar.Brand href="/home">Muscle Track</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/home">Home</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
