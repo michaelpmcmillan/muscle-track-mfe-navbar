a2a5f8f238b407cbb3b4f6aebe349c334fdafd0a
Michael.McMillan <michael.mcmillan@musclefood.com>
2020-10-22 16:38:37 +0100

    Change a to Nav.Link

--- src/navbar-links/logout.tsx
+++ src/navbar-links/logout.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 import { logout, isLoggedIn } from "../plugin/identity";
+import Nav from "react-bootstrap/Nav";
 
 export default function Logout() {
   return (
     <>
       {isLoggedIn() && (
-        <a className="nav-link" onClick={logout} href="#">
+        <Nav.Link onClick={logout}>
           <FontAwesomeIcon icon={faSignOutAlt} /> Logout
-        </a>
+        </Nav.Link>
       )}
     </>
   );
